03c1b339321b90d90743f3549f7637c059fabaa8
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2025-12-01 17:49:15 +0300

    Delivery Manager -> UI part one

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -9,6 +9,9 @@ public class DeliveryCounter : BaseCounter
             if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
             {
                 //only accept Plates
+
+                DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
+                
                 player.GetKitchenObject().DestroySelf();
             }
         }
---
+++ Assets/Scripts/ScriptableObjects/RecipeSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu()]
+public class RecipeSO : ScriptableObject
+{
+    public List<KitchenObjectSO> kitchenObjectSOList;
+    public string recipeName;
+}

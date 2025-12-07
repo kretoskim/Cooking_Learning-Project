fd0d61191226c1b248ed5e57daf5b497f6565219
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2025-12-07 10:28:48 +0300

    Pause & Clear Statics

--- Assets/Scripts/Counters/BaseCounter.cs
+++ Assets/Scripts/Counters/BaseCounter.cs
@@ -4,6 +4,10 @@ using System;
 public class BaseCounter : MonoBehaviour, IKitchenObjectParent
 {
     public static event EventHandler OnAnyObjectPlacedHere;
+    public static void ResetStaticData()
+    {
+        OnAnyObjectPlacedHere = null;
+    }
     [SerializeField] private Transform counterTopPoint;
     
     private KitchenObject kitchenObject;

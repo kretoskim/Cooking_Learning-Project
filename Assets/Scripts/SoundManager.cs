92bb890a8fad343768cd68f6bd8c46951a76b977
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2025-12-07 12:26:16 +0300

    Options & Audio Levels

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
     public static SoundManager Instance { get; private set; }
     [SerializeField] private AudioClipsRefSO audioClipsRefSO;
+    [SerializeField] private int volumeMultiplier = 1;
+    private float volume = 1f;
 
     private void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
     private void Start()
     {
@@ -61,10 +66,24 @@ public class SoundManager : MonoBehaviour
 
     private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
     public void PlayFootstepsSound(Vector3 position, float volume)
     {
         PlaySound(audioClipsRefSO.footstep, position, volume);
+    }
+    public void ChangeVolume()
+    {
+        volume += 0.1f;
+        if(volume > 1f)
+        {
+            volume = 0f;
+        }
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+    public float GetVolume()
+    {
+        return volume;
     }
 }
